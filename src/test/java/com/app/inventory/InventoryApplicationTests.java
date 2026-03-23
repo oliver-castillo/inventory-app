c6ac0b340e12fbea342277d326bff317da695a1a
Oliver Castillo <olcastillot@gmail.com>
2026-03-22 22:36:53 -0500

    refactor: rename application and test classes, update package structure, and add dependencies

--- src/test/java/com/app/inventory/InventoryApplicationTests.java
+++ src/test/java/com/app/inventory/InventoryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.app.inventory_app;
+package com.app.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InventoryAppApplicationTests {
+class InventoryApplicationTests {
 
 	@Test
 	void contextLoads() {
