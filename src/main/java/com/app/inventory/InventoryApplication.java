1b2ed641e720765f4a61a555b379b3bd8b3a45f3
Oliver Castillo <olcastillot@gmail.com>
2026-03-22 22:37:42 -0500

    style: format code for consistency in InventoryApplication and tests

--- src/main/java/com/app/inventory/InventoryApplication.java
+++ src/main/java/com/app/inventory/InventoryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(InventoryApplication.class, args);
-	}
+  static void main(String[] args) {
+    SpringApplication.run(InventoryApplication.class, args);
+  }
 
 }
