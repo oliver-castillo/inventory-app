c1b57ea401ae0d299d597e7e70abc829d158cde8
Oliver Castillo <olcastillot@gmail.com>
2026-04-14 22:38:26 -0500

    feat: enhance UserController to implement user creation with UserService and UserMapper

--- src/main/java/com/app/inventory/controller/UserController.java
+++ src/main/java/com/app/inventory/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.app.inventory.controller;
 
+import com.app.inventory.domain.model.User;
+import com.app.inventory.domain.service.UserService;
+import com.app.inventory.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.openapitools.api.UsersApi;
-import org.openapitools.model.User;
 import org.openapitools.model.UserRequest;
+import org.openapitools.model.UserResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController implements UsersApi {
+  private final UserService userService;
+  private final UserMapper userMapper;
+
   @Override
-  public ResponseEntity<User> createUser(UserRequest userRequest) {
-    return UsersApi.super.createUser(userRequest);
+  public ResponseEntity<UserResponse> createUser(UserRequest userRequest) {
+    User createdUser = userService.createUser(userMapper.userRequestToUser(userRequest));
+    return new ResponseEntity<>(userMapper.userToUserResponse(createdUser), HttpStatus.CREATED);
   }
 }
